72a60f979252fd7adc42d97088ce7f8da88dfbdc
Sagharyzdni <sagharyazdani2000@gmail.com>
2024-01-08 11:40:15 +0100

    first-commit-test

---
+++ chai-example-main/chai-example-main/src/app.js
@@ -0,0 +1,43 @@
+class Cube {
+    constructor(length) {
+        this.length = length;
+    }
+    
+    getSideLength () {
+        return this.length;
+    }
+    
+    getSurfaceArea () {
+        return (this.length * this.length) * 6;
+    }
+    
+    getVolume () {
+        return Math.pow(this.length,3);
+    }
+}
+
+class Panier{
+    constructor(){
+        this.article = [];
+        this.montant = 0;
+    }
+
+    addArticle(article,montant){
+        this.article.push(article);
+        this.montant += montant;
+    }
+
+    removeArticle(artice,montant){
+        this.article.pop(artice);
+        this.montant -= montant;
+    }
+
+}
+
+
+
+
+module.exports = {
+    Cube:Cube,
+    Panier:Panier
+}
